1f7fc27688d19465614adcd60178bfe84c50fd81
cdtest <corydavis1989@gmail.com>
2022-01-31 16:38:24 -0600

    database is connected and working

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const mysql = require('mysql2');
+
 const PORT = process.env.PORT || 3001;
 const app = express();
 
@@ -6,15 +8,26 @@ const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-app.get('/', (req, res) => {
-    res.json({
-        message: 'Hello World'
-    });
+// Connect to database
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        user: 'root',
+        password: 'Snareline1',
+        database: 'election'
+    },
+    console.log('Connected to the election database.')
+);
+
+db.query(`SELECT * FROM candidates`, (err, rows) => {
+    console.log(rows);
+})
+
+// route to handle user requests that aren't supported by the app
+// catchall route
+app.use((req, res) => {
+    res.status(404).end();
 });
-
-
-
-
 
 // connection function that will start the express server on port 3001
 app.listen(PORT, () => { 
